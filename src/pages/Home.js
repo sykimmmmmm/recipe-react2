dfc740014ddbc4c1c07d6948aec8cf0183c1f4b7
novice <seungyeon5374@gmail.com>
2024-06-13 10:21:29 +0900

    db등록연습

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,31 @@ export default function Home(){
     const [user,setUser] = useState(null)
     useEffect(()=>{
         
-        fetch(`${process.env.REACT_APP_BASE_URL}`)
-        .then(res=> res.text())
+        fetch(`${process.env.REACT_APP_BASE_URL}/users/register`,{
+            headers:{'Content-Type':'application/json'},
+            method:'POST',
+            body:JSON.stringify({
+                name:'하늘',
+                email:'sky2@gmail.com',
+                password:'sky123!@#',
+                userId:'sky2'
+            })
+        })
+        .then(res=> res.json())
         .then(result=>{
             console.log(user)
-            setUser(result)})  
+            setUser(result.user)})  
     },[])
     return (
         <>
-            {user ? <h1>{user}</h1> : '로딩중...'}
+            {user ? (
+            <>
+            <h1>회원정보</h1>
+            <p>이름: {user.name}</p>
+            <p>연락처: {user.email}</p>
+            <p>아이디: {user.userId}</p>
+            </>
+            ) : "사용자정보 조회중..."}
             {/* <RecipeList/> */}
         </>
     )
